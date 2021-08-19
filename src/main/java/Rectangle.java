4f7b43750c2bd34301a177db32a31a6921a634b6
kowsikgelli <jaikrishnagelli@gmail.com>
2021-08-18 22:19:10 -0700

    Requirement 1:- Rectangle Area Test

--- src/main/java/Rectangle.java
+++ src/main/java/Rectangle.java
@@ -1,2 +1,12 @@
 public class Rectangle {
+    private int width;
+    private int length;
+    public Rectangle(int length, int width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public int area() {
+        return this.length*this.width;
+    }
 }
--- src/test/java/GeometricShapesTest.java
+++ src/test/java/GeometricShapesTest.java
@@ -1,2 +1,33 @@
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class GeometricShapesTest {
+    @Nested
+    public class RectangleAreaAndPerimeterTest{
+        @Test
+        void toCalculateAreaWhenLengthAndWidthArePositive(){
+            Rectangle rectangle = new Rectangle(4,2);
+            assertEquals(8,rectangle.area());
+        }
+
+        @Test
+        void toCalculateAreaWhenLengthAndWidthAreNegative(){
+            Rectangle rectangle = new Rectangle(-4,-2);
+            assertEquals(8,rectangle.area());
+        }
+
+        @Test
+        void toCalculateAreaWhenLengthIsPositiveAndWidthIsNegative(){
+            Rectangle rectangle = new Rectangle(4,-2);
+            assertEquals(-8,rectangle.area());
+        }
+
+        @Test
+        void toCalculateWhenLengthIsNegativeAndWidthIsPositive(){
+            Rectangle rectangle = new Rectangle(-4,2);
+            assertEquals(-8,rectangle.area());
+        }
+    }
 }
